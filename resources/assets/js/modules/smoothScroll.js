b8860e31250dbc9b48203b1258818642de81e183
Lindsey Bradford <lindseyb@wearetelegraph.com>
2018-04-04 10:39:29 -0500

    + adds fonts

--- resources/assets/js/modules/smoothScroll.js
+++ resources/assets/js/modules/smoothScroll.js
@@ -2,7 +2,7 @@
 // Example Exports for simple functions:
 //
 export default function smooth() {
-	console.log('hi')
+
 	var SmoothScroll = require("../vendor/smooth-scroll.js");
 	const scroll = new SmoothScroll('a[href*="#"]', {
 			speed: 750,
--- webpack.mix.js
+++ webpack.mix.js
@@ -7,6 +7,7 @@ mix.setPublicPath('./');
 mix
 	.sass('resources/assets/sass/app.scss', 'public/assets/css/app.css')
 	.js('resources/assets/js/app.js', 'public/assets/js/app.js')
+	.copyDirectory('resources/assets/fonts', 'public/assets/fonts')
 	.copyDirectory('resources/assets/static', 'public/assets/static')
 	.copyDirectory('resources/assets/img', 'public/assets/img')
 	.copyDirectory('resources/assets/svg', 'public/assets/svg')
