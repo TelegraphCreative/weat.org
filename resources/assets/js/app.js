f37b939eb15d4223a636b67ed75b5c012dcdbc8e
Lindsey Bradford <lindseyb@wearetelegraph.com>
2018-04-03 20:57:47 -0500

    im+ added import, require examples + added lazysizes

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,9 +2,32 @@
 // WEAT, Built by Telegraph
 //
 
+//
+// # On Call Imports (called with `data-module` attr. as needed)
+//
 import './modules'
 
-console.log('WEAT, Built by Telegraph');
+//
+// # Global Imports/Requires (need all the time)
+//
 
-// document.addEventListener('DOMContentLoaded', function () {
-// });
+// ## Smooth Scroll
+import smooth from './modules/smoothScroll';
+smooth();
+
+// ## Lazysizes (Images)
+require("./vendor/lazysizes.js");
+
+//
+// # Examples:
+//
+// // ## Simple function export example:
+// // import {smooth} from './modules/example-simple-function-export.js'
+
+// // ## Export default function: 
+// // import smooth from './modules/smoothScroll';
+
+// // ## Require an entire file:
+// // require("./vendor/lazysizes.js");
+
+console.log('WEAT, Built by Telegraph');
